4157b980465b70558ec98dc7dfac198a83a2c9cd
NogiTamu <76116273+NogiTamu@users.noreply.github.com>
2022-07-01 11:34:02 +0900

    da

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,13 +1,19 @@
 import { FC, memo } from "react";
 import { Route, Routes } from "react-router-dom";
+import { Home } from "../components/pages/Home";
 import { Login } from "../components/pages/Login";
-import { homeRoutes } from "HomeRoutes";
+import { Setting } from "../components/pages/Setting";
+import { UserManagement } from "../components/pages/UserManagement";
+import { Page404 } from "../components/pages/Page404";
 
 export const Router: FC = memo(() => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/home" render={() => <Routes>{homeRoutes}</Routes>} />
+      <Route path="home" element={<Home />} />
+      <Route path="home/user_management" element={<UserManagement />} />
+      <Route path="home/setting" element={<Setting />} />
+      <Route path="*" element={<Page404 />} />
     </Routes>
   );
 });
